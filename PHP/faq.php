2c6987461d661942ff405f189cdfc26df05c05ae
Nimouh <simaoaugusto11@hotmail.com>
2022-05-23 20:50:15 +0100

    fix faq error

--- PHP/faq.php
+++ PHP/faq.php
@@ -16,14 +16,14 @@ echo " <div class='container' style='padding-top: 100px!important;'>
                     <button class='accordion-button' type='button' data-bs-toggle='collapse'
                             data-bs-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
                         Posso imprimir os questionários?
-</button>
+                    </button>
                 </h2>
                 <div id='collapseOne' class='accordion-collapse collapse show' aria-labelledby='headingOne'
                      data-bs-parent='#accordionExample'>
                     <div class='accordion-body'>
-                        <strong>Sim</strong>, é possível imprimir qualquer um dos questionários individuais. 
-                        Os questionários impressos fazem o entretenimento ideal em viagens longas, 
-                        reuniões familiares e alturas em que um computador não está à mão. 
+                        <strong>Sim</strong>, é possível imprimir qualquer um dos questionários individuais.
+                        Os questionários impressos fazem o entretenimento ideal em viagens longas,
+                        reuniões familiares e alturas em que um computador não está à mão.
                         E o tempo todo, os alunos consolidam a sua aprendizagem escolar!
                     </div>
                 </div>
@@ -32,17 +32,22 @@ echo " <div class='container' style='padding-top: 100px!important;'>
                 <h2 class='accordion-header' id='headingTwo'>
                     <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse'
                             data-bs-target='#collapseTwo' aria-expanded='false' aria-controls='collapseTwo'>
-    Os Quizzes estão protegidos por direitos de autor?
-</button>
+                        Os Quizzes estão protegidos por direitos de autor?
+                    </button>
                 </h2>
                 <div id='collapseTwo' class='accordion-collapse collapse' aria-labelledby='headingTwo'
                      data-bs-parent='#accordionExample'>
                     <div class='accordion-body'>
-                        <strong>Sim</strong>, todos os nossos questionários estão sujeitos a restrições de direitos de autor, 
-                        mas permitimos aos nossos subscritores imprimir os questionários; desde que sejam reproduzidos na 
-                        forma precisa que o nosso ‘software’ gera (cada folha deve mostrar os detalhes dos questionários educativos no final do questionário). 
-                        A utilização de qualquer um dos materiais sem mostrar os detalhes dos Testes de Educação é uma violação dos nossos direitos de autor. 
-                        Serão tomadas medidas legais contra qualquer escola que utilize os questionários sem a nossa autorização expressa. 
+                        <strong>Sim</strong>, todos os nossos questionários estão sujeitos a restrições de direitos de
+                        autor,
+                        mas permitimos aos nossos subscritores imprimir os questionários; desde que sejam reproduzidos
+                        na
+                        forma precisa que o nosso ‘software’ gera (cada folha deve mostrar os detalhes dos questionários
+                        educativos no final do questionário).
+                        A utilização de qualquer um dos materiais sem mostrar os detalhes dos Testes de Educação é uma
+                        violação dos nossos direitos de autor.
+                        Serão tomadas medidas legais contra qualquer escola que utilize os questionários sem a nossa
+                        autorização expressa.
                         Afinal de contas, temos de ganhar a vida!
                     </div>
                 </div>
@@ -51,46 +56,46 @@ echo " <div class='container' style='padding-top: 100px!important;'>
                 <h2 class='accordion-header' id='headingThree'>
                     <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse'
                             data-bs-target='#collapseThree' aria-expanded='false' aria-controls='collapseThree'>
-    Como seleciono um Quiz para jogar?
-</button>
+                        Como seleciono um Quiz para jogar?
+                    </button>
                 </h2>
                 <div id='collapseThree' class='accordion-collapse collapse' aria-labelledby='headingThree'
                      data-bs-parent='#accordionExample'>
                     <div class='accordion-body'>
-                        Quando clicar numa categoria de quiz, 
-                        aparecerá outra página que mostra os títulos do quiz - clique no botão 'Jogar' ao 
+                        Quando clicar numa categoria de quiz,
+                        aparecerá outra página que mostra os títulos do quiz - clique no botão 'Jogar' ao
                         lado do quiz específico que deseja jogar
                     </div>
                 </div>
             </div>
             <div class='accordion-item'>
-                <h2 class='accordion-header' id='headingThree'>
+                <h2 class='accordion-header' id='headingFour'>
                     <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse'
-                            data-bs-target='#collapseThree' aria-expanded='false' aria-controls='collapseThree'>
-    PERGUNTA 4
-</button>
+                            data-bs-target='#collapseFour' aria-expanded='false' aria-controls='collapseFour'>
+                        PERGUNTA 4
+                    </button>
                 </h2>
-                <div id='collapseThree' class='accordion-collapse collapse' aria-labelledby='headingThree'
+                <div id='collapseFour' class='accordion-collapse collapse' aria-labelledby='headingFour'
                      data-bs-parent='#accordionExample'>
                     <div class='accordion-body'>
-                        Quando clicar numa categoria de quiz, 
-                        aparecerá outra página que mostra os títulos do quiz - clique no botão 'Jogar' ao 
+                        Quando clicar numa categoria de quiz,
+                        aparecerá outra página que mostra os títulos do quiz - clique no botão 'Jogar' ao
                         lado do quiz específico que deseja jogar
                     </div>
                 </div>
             </div>
             <div class='accordion-item'>
-                <h2 class='accordion-header' id='headingThree'>
+                <h2 class='accordion-header' id='headingFive'>
                     <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse'
-                            data-bs-target='#collapseThree' aria-expanded='false' aria-controls='collapseThree'>
-    PERGUNTA 5
-</button>
+                            data-bs-target='#collapseFive' aria-expanded='false' aria-controls='collapseFive'>
+                        PERGUNTA 5
+                    </button>
                 </h2>
-                <div id='collapseThree' class='accordion-collapse collapse' aria-labelledby='headingThree'
+                <div id='collapseFive' class='accordion-collapse collapse' aria-labelledby='headingFive'
                      data-bs-parent='#accordionExample'>
                     <div class='accordion-body'>
-                        Quando clicar numa categoria de quiz, 
-                        aparecerá outra página que mostra os títulos do quiz - clique no botão 'Jogar' ao 
+                        Quando clicar numa categoria de quiz,
+                        aparecerá outra página que mostra os títulos do quiz - clique no botão 'Jogar' ao
                         lado do quiz específico que deseja jogar
                     </div>
                 </div>
@@ -98,7 +103,7 @@ echo " <div class='container' style='padding-top: 100px!important;'>
         </div>
     </div>
     <div class='row white-box'></div>
-</div> ";
+</div>";
 
 // include footer.php
 include('footer.php');
